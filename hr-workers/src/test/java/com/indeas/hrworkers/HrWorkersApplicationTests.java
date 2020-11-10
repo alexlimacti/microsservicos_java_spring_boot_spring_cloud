eae342af46bb7fce81bdc99862fb6e3cbc4c8d83
Alexsandro de Lima Silva <alexlima_cti@hotmail.com>
2020-11-10 08:29:56 -0300

    inicio do modulo hr-work

---
+++ hr-workers/src/test/java/com/indeas/hrworkers/HrWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indeas.hrworkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
